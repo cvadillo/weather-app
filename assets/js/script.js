f9b45ab64d5d1a7e1df04d58c619b7c276bc900c
Carlos V <cavarod@gmail.com>
2021-03-24 13:00:46 -0700

    finished all display and html mockups. Starting the API integration now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,27 @@
 // Initialize Materialize CSS
 M.AutoInit();
-M.updateTextFields()
 
-// add event listener to make sure that the input goes from active to inactive
+
+
+
+
+
+
+// Resize cards on smaller windows
+$(window).resize(function(){
+
+		if($(window).width() <= 860){
+		$('.horizontal').removeClass('horizontal');
+		}
+		if(860 <= $(window).width()){
+			$('.main-weather').addClass('horizontal');
+		}
+
+		if($(window).width() <= 1200){
+		$('.daily').removeClass('m2');
+		}
+		if(1200 <= $(window).width()){
+			$('.daily').addClass('m2');
+		}
+
+    });
