cc89d98af3f6a3a672dc4e798893991322495ffc
Carlos V <cavarod@gmail.com>
2021-03-24 15:01:04 -0700

    enabled javascript on all forms and submissions, tested the initial current weather endpoint for the API, everything is working smoothly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,52 @@
 // Initialize Materialize CSS
 M.AutoInit();
 
+// DOM Element Variables
+citySearchEl = document.getElementById('city-search');
+mainEl = document.getElementById('main')
+submitButtonEl = document.getElementById('city-submit');
+
+// Search variables
+let cityQuery = '';
+
+// Weatherstack API URL and KEY
+const weatherURL = 'http://api.weatherstack.com/';
+const weatherKey = '6c2a44a6bfac6c49aad8e871d365fb56';
+
+// test fetch function
+const getCurrentWeather = function (url, key, query) {
+
+	// set the url and the key to a search
+	const searchURL = `${url}current?access_key=${key}&query=${cityQuery}`;
+	fetch(searchURL).then(function(response) {
+		response.json().then(function(data) {
+			let currentWeather = data;
+			console.log(currentWeather);
+		})
+	})
+};
 
 
+const removeEl = () => {
+	mainEl.innerHTML = '';
+};
 
+// Event Listener functions
+citySearchEl.addEventListener('onsubmit', function(event) {
+	event.preventDefault();
+	cityQuery = citySearchEl.value;
+	removeEl();
+ 	console.log(cityQuery);
+ 	getCurrentWeather(weatherURL, weatherKey, cityQuery);
+});
 
+submitButtonEl.addEventListener('click', function(event) {
+	event.preventDefault();
+	cityQuery = citySearchEl.value
+	removeEl();
+	console.log(cityQuery);
+	getCurrentWeather(weatherURL, weatherKey, cityQuery);
+})
 
 
 // Resize cards on smaller windows
