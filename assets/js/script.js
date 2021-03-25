bc3339b8236a98118645267e4fc0ceb480fd82e0
Carlos V <cavarod@gmail.com>
2021-03-24 21:48:57 -0700

    Able to ping the servers and recieve the data

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,20 @@
 M.AutoInit();
 
 // DOM Element Variables
-citySearchEl = document.getElementById('city-search');
-mainEl = document.getElementById('main')
-submitButtonEl = document.getElementById('city-submit');
+const citySearchEl = document.getElementById('city-search');
+const mainWeatherEl = document.getElementById('weather-render')
+const submitButtonEl = document.getElementById('city-submit');
 
 // Search variables
 let cityQuery = '';
+let currentWeather = '';
+let forecastWeather = '';
+let cityImage = '';
 
 // Weatherstack API URL and KEY
 const weatherURL = 'http://api.weatherstack.com/';
 const weatherKey = '6c2a44a6bfac6c49aad8e871d365fb56';
+const unsplashApiKey = 'bJlzPHCI0vahqxnDYHw17P2WKMEE4PZqbrZ9Uop1wzU'
 
 // test fetch function
 const getCurrentWeather = function (url, key, query) {
@@ -20,42 +24,62 @@ const getCurrentWeather = function (url, key, query) {
 	const searchURL = `${url}current?access_key=${key}&query=${cityQuery}`;
 	fetch(searchURL).then(function(response) {
 		response.json().then(function(data) {
-			let currentWeather = data;
+			currentWeather = data;
 			console.log(currentWeather);
 		})
 	})
 };
 
+const getForecast = function(url, key, query) {
 
-const removeEl = () => {
-	mainEl.innerHTML = '';
+	// set the url and the key to a search
+	const searchURL = `${url}forecast?access_key=${key}&query=${cityQuery}`;
+	fetch(searchURL).then(function(response) {
+		response.json().then(function(data) {
+			forecastWeather = data;
+			console.log(forecastWeather);
+		})
+	})
+}
+
+const getCityImage = function(query) {
+	const unsplashApiUrl =`https://api.unsplash.com/search/photos?client_id=${unsplashApiKey}&query=${cityQuery}`;
+	fetch(unsplashApiUrl).then(function(response){
+		response.json().then(function(data){
+			cityImage = data;
+			console.log(cityImage);
+		})
+	})
 };
 
-// Event Listener functions
-citySearchEl.addEventListener('onsubmit', function(event) {
+const removeEl = function() {
+	mainWeatherEl.innerHTML = '';
+};
+
+const submitting = function(event) {
 	event.preventDefault();
 	cityQuery = citySearchEl.value;
-	removeEl();
  	console.log(cityQuery);
+ 	removeEl();
  	getCurrentWeather(weatherURL, weatherKey, cityQuery);
-});
+ 	getForecast(weatherURL, weatherKey, cityQuery);
+ 	getCityImage(cityQuery);
+};
 
-submitButtonEl.addEventListener('click', function(event) {
-	event.preventDefault();
-	cityQuery = citySearchEl.value
-	removeEl();
-	console.log(cityQuery);
-	getCurrentWeather(weatherURL, weatherKey, cityQuery);
-})
+
+// Event Listener functions
+citySearchEl.addEventListener('onsubmit', submitting);
+
+submitButtonEl.addEventListener('click', submitting);
 
 
 // Resize cards on smaller windows
 $(window).resize(function(){
 
-		if($(window).width() <= 860){
+		if($(window).width() <= 1244){
 		$('.horizontal').removeClass('horizontal');
 		}
-		if(860 <= $(window).width()){
+		if(1243 <= $(window).width()){
 			$('.main-weather').addClass('horizontal');
 		}
 
